b268a5d23884fe38f55787926197d5f5518251a4
ReenuLK <reenugowda01@gmail.com>
2026-04-23 22:34:01 +0530

    Update notebook for improving scores

--- notebook/setup_models.py
+++ notebook/setup_models.py
@@ -2,6 +2,6 @@ from huggingface_hub import snapshot_download
 
 snapshot_download(
     repo_id="sentence-transformers/all-MiniLM-L6-v2",
-    local_dir="./notebook/models/all-MiniLM-L6-v2",
+    local_dir="./models/all-MiniLM-L6-v2",
     local_dir_use_symlinks=False
 )
